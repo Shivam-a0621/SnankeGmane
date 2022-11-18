2f45a4ee19453cf0c9d5a0318866dcda66be38a6
shivamak2003 <bhardwajshivam.2108@gmail.com>
2022-11-19 02:06:26 +0530

    Fourth commit

---
+++ day20Snake_game.py
@@ -0,0 +1,44 @@
+from multiprocessing.resource_sharer import stop
+from tkinter import W
+from tracemalloc import start
+from turtle import Turtle, Screen, bgcolor, update
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+screen = Screen()
+
+
+screen.setup(width=600, height=600)
+
+
+screen.bgcolor("black")
+screen.title("The snack game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard=Scoreboard()
+
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move( )
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score ()
+    # detect collision with wall
+    if snake.head.xcor()>280 or snake.head.xcor() <-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
+        game_is_on = False
+        scoreboard.game_over()
+
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+ALIGEMENT="center"
+FONT=("Arial",24,"normal")
+
+class Scoreboard(Turtle):
+    def __init__(self): 
+         super().__init__()
+         self.score = 0
+         self.color("white")
+         self.penup()
+         self.goto(0,268) 
+         
+         self.hideturtle()
+         self.update_scoreboard()
+    
+    def update_scoreboard(self):
+        self.write(f"Score:{self.score}",align=ALIGEMENT,font=FONT)
+    
+    
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER",align=ALIGEMENT,font=FONT)
+    
+    def increase_score(self) :
+        self.score+=1
+        self.clear()
+        self.update_scoreboard()  
